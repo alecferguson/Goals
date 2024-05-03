d9e3b3416fd708b6d5d8cdb81abc6ec76ec080c4
Alec Ferguson <160795559+alecferguson@users.noreply.github.com>
2024-05-03 16:03:00 -0700

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware') //Pulling custom error handler
+const connectDB = require('./config/db')
 const port = process.env.PORT  || 5000 //Opens port located env or 5000 if it does not work.
-
-const app = express() //Creating variable express and init
+//Adding connection to MongoDB
+connectDB()
+//Creating variable express and init
+const app = express()
 //Adding middleware to read body
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
